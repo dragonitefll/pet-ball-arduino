686d3b53093ea62f09f4914ac89c5feb58914ea1
Benjamin Liu <nj.benliu@gmail.com>
2016-10-16 15:57:57 -0400

    Added a bunch code to make an LED "move" across an LED strip.

--- pet-ball-arduino.ino
+++ pet-ball-arduino.ino
@@ -1,13 +1,22 @@
 #include <Servo.h>
 
 #include "FastLED.h"
-
+#define NUM_LEDS 10
+#define DATA_PIN 6
+CRGBleds[NUM_LEDS];
 void setup() {
   // put your setup code here, to run once:
-
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB::Pink;
+            FastLED.show();
+            // clear this led for the next time around the loop
+            leds[dot] = CRGB::Black;
+            delay(30);
+        }
 
 }
