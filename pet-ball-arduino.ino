a2199ba96e9444b50871fcd6d2c803921add876e
anli5005 <li.anthony917@gmail.com>
2016-10-30 12:33:55 -0400

    Add a serial DC motor interface

--- pet-ball-arduino.ino
+++ pet-ball-arduino.ino
@@ -17,6 +17,8 @@ void setup() {
 
   leftWheel.attach(2, 3, 9);
   rightWheel.attach(4, 5, 10);
+
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -28,5 +30,37 @@ void loop() {
     leds[dot] = CRGB::Black;
     delay(30);
   }
-
+  
+  if (Serial.available() > 0) {
+    int command = Serial.read();
+    if (command == 1) {
+      char args[3] = {0, 0, 0};
+      Serial.readBytes(args, 3);
+      driveCommand(args[0], args[1], args[2]);
+    }
+  }
 }
+
+void driveCommand(unsigned char mode, unsigned char speedA, unsigned char speedB) {
+  if (bitRead(mode, 0)) {
+    if (bitRead(mode, 1)) {
+      leftWheel.start((bitRead(mode, 2) ? -1 : 1) * speedA); 
+    }
+  } else {
+    leftWheel.stop();
+  }
+  if (bitRead(mode, 3)) {
+    if (bitRead(mode, 4)) {
+      rightWheel.start((bitRead(mode, 5) ? -1 : 1) * speedB); 
+    }
+  } else {
+    rightWheel.stop();
+  }
+  sendOk();
+}
+
+void sendOk() {
+  Serial.write(0);
+  Serial.flush();
+}
+
