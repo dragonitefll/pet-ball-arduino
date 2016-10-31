2aada214b7df2218d4a89b32e2f504849b893c6a
Benjamin Liu <nj.benliu@gmail.com>
2016-10-31 18:17:47 -0400

    Added code from master branch

--- pet-ball-arduino.ino
+++ pet-ball-arduino.ino
@@ -67,4 +67,36 @@ void loop() {
       }
     }
     //Serial.println("exit loop");
+
+     if (Serial.available() > 0) {
+    int command = Serial.read();
+    if (command == 1) {
+      char args[3] = {0, 0, 0};
+      Serial.readBytes(args, 3);
+      driveCommand(args[0], args[1], args[2]);
+    }
+  }
 } 
+
+void driveCommand(unsigned char mode, unsigned char speedA, unsigned char speedB) {
+  if (bitRead(mode, 0)) {
+    if (bitRead(mode, 1)) {
+      leftWheel.start((bitRead(mode, 2) ? -1 : 1) * speedA); 
+    }
+  } else {
+    leftWheel.stop();
+  }
+  if (bitRead(mode, 3)) {
+    if (bitRead(mode, 4)) {
+      rightWheel.start((bitRead(mode, 5) ? -1 : 1) * speedB); 
+    }
+  } else {
+    rightWheel.stop();
+  }
+  sendOk();
+}
+
+void sendOk() {
+  Serial.write(0);
+  Serial.flush();
+}
